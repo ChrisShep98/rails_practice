aae9025ea485fab04c2dc95c99ee27df0aec64aa
ChrisShep98 <shepardchristopher1@gmail.com>
2025-11-13 18:47:19 -0500

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :entries
+  root to: "entries#index" # This sets up our root route and will go to the entries controller and find the index method and route to that
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
