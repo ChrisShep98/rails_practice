5c75038c346c9d1193816882544c122206a3df2c
ChrisShep98 <shepardchristopher1@gmail.com>
2025-11-17 14:31:45 -0500

    model test

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,8 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Dinner", carbohydrates: 50, proteins: 25, fats: 30, calories: 400)
+    assert entry.save
+  end
 end
