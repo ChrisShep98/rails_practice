3e5a0356071a2544cb446884f11fc2a7985ab4d0
ChrisShep98 <shepardchristopher1@gmail.com>
2025-11-17 15:59:49 -0500

    migration run

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_11_13_221450) do
+ActiveRecord::Schema.define(version: 2025_11_17_203447) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string "meal_type"
