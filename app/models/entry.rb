6cb39857a3101fd5056f00009e9a085a403c3205
ChrisShep98 <shepardchristopher1@gmail.com>
2025-11-17 11:55:42 -0500

    added to archives index page, created index method in archives controller which gets all posts by day

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+  def day
+    self.created_at.strftime("%b %e, %Y") #can refer to them model here as self. Could also use the class name Entry, will do same thing
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'archives/index'
-  get 'archive/index'
   resources :entries
   root to: "entries#index" # This sets up our root route and will go to the entries controller and find the index method and route to that
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
