1f75327bab1be6c1fb04599031d4d5f889a1ccdf
ChrisShep98 <shepardchristopher1@gmail.com>
2025-11-17 18:14:00 -0500

    updated models to reflect table relationship, updated entries form, added category_id to entry_params .permit method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :entries # One category can have many entries
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ApplicationRecord
 
-  validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true # these fields must not be blank
+  belongs_to :category
+  validates :calories, :proteins, :carbohydrates, :fats, :meal_type, :category_id, presence: true # these fields must not be blank
 
   def day
     self.created_at.strftime("%b %e, %Y") #can refer to them model here as self. Could also use the class name Entry, will do same thing
