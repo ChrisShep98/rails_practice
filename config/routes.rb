5ffc94552a580250ba79a5328462881c5d8015a1
ChrisShep98 <shepardchristopher1@gmail.com>
2025-12-03 16:16:20 -0500

    changed default root view to song_versions index instead of entries

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :categories
   get 'archives/index'
   resources :entries
-  root to: "entries#index" # This sets up our root route and will go to the entries controller and find the index method and route to that
+  root to: "song_versions#index" # This sets up our root route and will go to the song_version controller and find the index method and route to that
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
